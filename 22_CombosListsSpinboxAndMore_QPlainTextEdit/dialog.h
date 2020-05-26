94e2dc5f173a3f8ea8b956656156bcf7a765051c
George Calin <george.calin@gmail.com>
2020-05-26 17:51:49 +0300

    Update dialog.h

--- 22_CombosListsSpinboxAndMore_QPlainTextEdit/dialog.h
+++ 22_CombosListsSpinboxAndMore_QPlainTextEdit/dialog.h
@@ -2,6 +2,12 @@
 #define DIALOG_H
 
 #include <QDialog>
+#include <QPushButton>
+#include <QPlainTextEdit>
+#include <QMessageBox>
+#include <QFile>
+#include <QTextStream>
+
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class Dialog; }
@@ -15,7 +21,21 @@ public:
     Dialog(QWidget *parent = nullptr);
     ~Dialog();
 
+private slots:
+    void on_plainTextEdit_textChanged();
+
+    void on_buttonBox_clicked(QAbstractButton *button);
+    void load();
+    void save();
+
 private:
     Ui::Dialog *ui;
+    bool m_saved;
+
+
+
+    // QWidget interface
+protected:
+    void closeEvent(QCloseEvent *event) override;
 };
 #endif // DIALOG_H
