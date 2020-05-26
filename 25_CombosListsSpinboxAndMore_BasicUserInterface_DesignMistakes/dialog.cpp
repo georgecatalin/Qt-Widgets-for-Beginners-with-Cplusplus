525a229d9434aa907f95a3cde588cbae44ac5b79
georgecatalin <george.calin@gmail.com>
2020-05-26 23:32:03 +0300

    Design mistakes in user interfaces built with Qt

---
+++ 25_CombosListsSpinboxAndMore_BasicUserInterface_DesignMistakes/dialog.cpp
@@ -0,0 +1,21 @@
+#include "dialog.h"
+#include "ui_dialog.h"
+
+Dialog::Dialog(QWidget *parent)
+    : QDialog(parent)
+    , ui(new Ui::Dialog)
+{
+    ui->setupUi(this);
+}
+
+Dialog::~Dialog()
+{
+    delete ui;
+}
+
+
+void Dialog::on_buttonBox_accepted()
+{
+    QMessageBox::information(this,"Volume","Volume= "+ QString::number(ui->spinBox->value()));
+    this->accept();
+}
